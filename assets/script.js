810f784ba5d822443462a00fdf7848108a4c7a6b
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-12 13:28:32 -0700

    first step of moving images works! modularizing now

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,6 @@
 var animalList = [];
 var activeAnimal = "";
 var answer;
-$(document).ready(function(){
-    $("#gif").on("click",function(){
-        var animal = $(this).attr("data-animal");
-        var movingURL = $(this).attr("image-live");
-        var stillURL = $(this).attr("image-still)")
-        if($(this).attr("image-state")==="still"){
-            $(this).attr("image-state","live")
-            $(this).attr("src",movingURL)
-        }
-        else{
-            $(this).attr("image-state","still")
-            $(this).attr("src",stillURL)
-        }
-    })
 
     /* CLICKING ON AN ANIMAL BUTTON 
     /* CREATING THE ANIMAL BUTTON FROM USER INPUT */
@@ -29,38 +15,47 @@ $(document).ready(function(){
         // reset search bar text
         showButtons();
     });
+   
 
+   function turnOn(img){
+        img.attr("src",img.attr("image-live"))
+   }
+   function turnOff(img){
+         img.attr("src",img.attr("image-still"))
+    }
     $("#animalLabels").on("click","button", function(){
         if(activeAnimal!=""){
             $("#animalImages").empty();
         }
         var animal = $(this).attr("data-animal").toLowerCase();
-            var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + animal + '&api_key=LunLEs3GAJawNrYS6bzD2RHiGCt35Iny&limit=15';
+        var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + animal + '&api_key=LunLEs3GAJawNrYS6bzD2RHiGCt35Iny&limit=10';
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
             var results = response.data;
-            var imageContainer = $("<div>");
+            var imageContainer = $("<div>")
             
             for(i in results){
                 var imageOutter = $("<div class='animal'>")
-                var animalImg = $("<img>")
+                var animalImg = $("<img id='gif'>")
                 animalImg.attr("src",results[i].images.fixed_height_still.url)
                 animalImg.attr("image-state","still");                                  
                 animalImg.attr("image-still",results[i].images.fixed_height_still.url)
                 animalImg.attr("image-live",results[i].images.fixed_height.url)
-                animalImg.attr("data-animal",animal)
-                var p = $("<p id='rating'>").text("Rating: " + results[i].rating.toUpperCase());
-     
+                animalImg.attr("data-animal",animal);
+
+            
+                var p = $("<p id='rating'>").text("Rated: " + results[i].rating.toUpperCase());
+
                 imageOutter.append(p);
                 imageOutter.append(animalImg);
-                $(imageContainer).prepend(imageOutter)
+                imageContainer.prepend(imageOutter)
             }
-            $("#animalImages").html(imageContainer);
+            $("#animalImages").html(imageContainer)
         })
     });
-})
+
 
 
 /* SHOWING BUTTONS WORKS */
@@ -75,3 +70,11 @@ function showButtons(){
     }
 }
 showButtons();
+
+$(document).ready(function(){
+    $("#animalImages").on("click","#gif",function(){
+        if($(this).attr("image-state")=="still"){
+            turnOn($(this))
+        }
+    })
+})
